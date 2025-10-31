823068f701cec73e3dacb3aa5145aad6c8c38826
Júlio César <juliocesarbatistadelima53@gmail.com>
2025-10-30 22:43:40 -0300

    algumas alterações

---
+++ src/main/java/br/com/juliocesar/todolist/user/UserModel.java
@@ -0,0 +1,11 @@
+package br.com.juliocesar.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    public String name;
+    public String username;
+    public String password;
+
+}
